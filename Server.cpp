91e2b8f3c87defdf746d864484eee69ba3262dae
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2023-12-23 03:32:10 +0900

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -1,48 +1,92 @@
+// server.cpp
 #include <iostream>
 #include <boost/asio.hpp>
 
 using namespace boost::asio;
 using ip::tcp;
 
-void handle_read(const boost::system::error_code& error, size_t bytes_transferred, std::array<char, 1024>& data, tcp::socket& socket) {
-    if (!error) {
-        std::cout << "Received data: " << std::string(data.data(), bytes_transferred) << std::endl;
+class Session : public std::enable_shared_from_this<Session> {
+public:
+    Session(tcp::socket socket) : socket_(std::move(socket)) {}
 
-        // Echo the received data back to the client
-        async_write(socket, buffer(data, bytes_transferred),
-            [&](const boost::system::error_code& write_error, size_t /*bytes_written*/) {
-                if (write_error) {
-                    std::cerr << "Error during write: " << write_error.message() << std::endl;
+    void start() {
+        doRead();
+    }
+
+private:
+    void doRead() {
+        auto self(shared_from_this());
+        socket_.async_read_some(
+            buffer(data_, max_length),
+            [this, self](boost::system::error_code ec, std::size_t length) {
+                if (!ec) {
+                    doWrite(length);
+                }
+                else {
+                    std::cerr << "Read error: " << ec.message() << "\n";
                 }
             });
     }
-    else {
-        std::cerr << "Error during read: " << error.message() << std::endl;
+
+    void doWrite(std::size_t length) {
+        auto self(shared_from_this());
+        async_write(
+            socket_,
+            buffer(data_, length),
+            [this, self](boost::system::error_code ec, std::size_t /* length */) {
+                if (!ec) {
+                    std::cout << "Received from client: " << data_ << "\n";
+                    doRead();
+                }
+                else {
+                    std::cerr << "Write error: " << ec.message() << "\n";
+                }
+            });
     }
-}
+
+    tcp::socket socket_;
+    enum { max_length = 1024 };
+    char data_[max_length];
+};
+
+class Server {
+public:
+    Server(io_service& io_service, short port)
+        : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
+        socket_(io_service) {
+        doAccept();
+    }
+
+private:
+    void doAccept() {
+        acceptor_.async_accept(
+            socket_,
+            [this](boost::system::error_code ec) {
+                if (!ec) {
+                    std::make_shared<Session>(std::move(socket_))->start();
+                }
+                else {
+                    std::cerr << "Accept error: " << ec.message() << "\n";
+                }
+
+                doAccept();
+            });
+    }
+
+    tcp::acceptor acceptor_;
+    tcp::socket socket_;
+};
 
 int main() {
     try {
-        boost::asio::io_service io_service;
+        io_service io_service;
+        Server server(io_service, 7777);
 
-        // Server
-        tcp::acceptor acceptor(io_service, tcp::endpoint(tcp::v4(), 7777));
-        tcp::socket server_socket(io_service);
-
-        std::cout << "Waiting for connection..." << std::endl;
-        acceptor.accept(server_socket);
-        std::cout << "Connection established." << std::endl;
-
-        std::array<char, 1024> data;
-
-        // Read data from the client
-        server_socket.async_read_some(buffer(data), boost::bind(handle_read, placeholders::error, placeholders::bytes_transferred, data, server_socket));
-
-        // Run the io_service to handle asynchronous operations
+        // 비동기 작업 처리
         io_service.run();
     }
     catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << std::endl;
+        std::cerr << "Exception: " << e.what() << "\n";
     }
 
     return 0;
