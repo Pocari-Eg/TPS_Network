02198f56aabc1203389568429f80a48d8015255d
Pocari-Eg <66935108+Pocari-Eg@users.noreply.github.com>
2023-12-24 21:47:32 +0900

    Update Server.cpp

--- Server.cpp
+++ Server.cpp
@@ -2,74 +2,61 @@
 #include <boost/asio.hpp>
 
 using namespace boost::asio;
-using ip::tcp;
 
-class MySession : public std::enable_shared_from_this<MySession> {
+class Server {
 public:
-    MySession(tcp::socket socket) : socket_(std::move(socket)) {}
-
-    void start() {
-        doRead();
+    Server(io_context& ioContext, unsigned short port)
+        : ioContext(ioContext), acceptor(ioContext, ip::tcp::endpoint(ip::tcp::v4(), port)) {
+        startAccept();
     }
 
 private:
-    void doRead() {
-        auto self(shared_from_this());
-        socket_.async_read_some(buffer(data_, max_length),
-            [this, self](boost::system::error_code ec, std::size_t length) {
+    void startAccept() {
+        // 새로운 소켓 생성
+        auto socket = std::make_shared<ip::tcp::socket>(ioContext);
+
+        // 비동기 연결 수락 시작
+        acceptor.async_accept(*socket,
+            [this, socket](boost::system::error_code ec) {
                 if (!ec) {
-                    doWrite(length);
+                    std::cout << "Accepted connection from: " << socket->remote_endpoint() << std::endl;
+
+                    // 클라이언트와의 통신을 위한 세션 시작
+                    startSession(socket);
                 }
+                else {
+                    std::cerr << "Accept error: " << ec.message() << std::endl;
+                }
+
+                // 다시 연결을 대기
+                startAccept();
             });
     }
 
-    void doWrite(std::size_t length) {
-        auto self(shared_from_this());
-        async_write(socket_, buffer(data_, length),
-            [this, self](boost::system::error_code ec, std::size_t /*length*/) {
-                if (!ec) {
-                    doRead();
-                }
-            });
-    }
-
-    tcp::socket socket_;
-    enum { max_length = 1024 };
-    char data_[max_length];
-};
-
-class MyServer {
-public:
-    MyServer(io_service& io_service, short port)
-        : acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-        socket_(io_service) {
-        doAccept();
+    void startSession(std::shared_ptr<ip::tcp::socket> socket) {
+        // 클라이언트와의 통신을 위한 비동기 작업을 여기에 추가
+        // 예를 들어, async_read, async_write 등을 사용할 수 있음
     }
 
 private:
-    void doAccept() {
-        acceptor_.async_accept(socket_,
-            [this](boost::system::error_code ec) {
-                if (!ec) {
-                    std::make_shared<MySession>(std::move(socket_))->start();
-                }
-                doAccept();
-            });
-    }
-
-    tcp::acceptor acceptor_;
-    tcp::socket socket_;
+    io_context& ioContext;
+    ip::tcp::acceptor acceptor;
 };
 
 int main() {
     try {
-        boost::asio::io_service io_service;
-        MyServer server(io_service,7777);
-        io_service.run();
-    }
-    catch (std::exception& e) {
-        std::cerr << "Exception: " << e.what() << "\n";
-    }
+        io_context ioContext;
+        unsigned short port = 7777;
 
-    return 0;
+        Server server(ioContext, port);
+
+        // 비동기 이벤트 처리 시작
+        ioContext.run();
+
+        return 0;
+    }
+    catch (const std::exception& e) {
+        std::cerr << "Exception: " << e.what() << std::endl;
+        return 1;
+    }
 }
